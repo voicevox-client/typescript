d4be12e3d6e41ae61fc1cab8ed9130de1ce4c0e3
mc_fdc <Masato.04.11.2007@gmail.com>
2023-03-28 22:50:09 +0900

    Fix: hostname

--- tests/basic.ts
+++ tests/basic.ts
@@ -1,6 +1,6 @@
 import Client from "voicevox-client"
 
-const client = new Client("http://localhost:50021")
+const client = new Client("http://127.0.0.1:50021")
 
 async function main() {
     const audioQuery = await client.createAudioQuery("こんにちは", 0, {});

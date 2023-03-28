06c9ada9ca3007046145aa9cc7822225e275b0b3
mc_fdc <Masato.04.11.2007@gmail.com>
2023-03-28 22:56:24 +0900

    Fix: 色々

--- tests/basic.ts
+++ tests/basic.ts
@@ -1,10 +1,12 @@
-import Client from "voicevox-client"
+import Client from "voicevox-client";
+import { promises as fs } from "fs";
 
-const client = new Client("http://127.0.0.1:50021")
+const client = new Client("http://127.0.0.1:50021");
 
 async function main() {
     const audioQuery = await client.createAudioQuery("こんにちは", 0, {});
-    await audioQuery.synthesis(0, {})
+    const data = await audioQuery.synthesis(0);
+    await fs.writeFile('sample.wav', data);
 }
 
-main()
+main();

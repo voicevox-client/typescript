2489897e64e4396e7d554a236981ef3036054e5c
mc_fdc <Masato.04.11.2007@gmail.com>
2023-05-18 17:34:36 +0900

    basic.ts を更新

--- tests/basic.ts
+++ tests/basic.ts
@@ -5,7 +5,7 @@ const client = new Client("http://127.0.0.1:50021");
 
 async function main() {
     const audioQuery = await client.createAudioQuery("こんにちは", 0, {});
-    await audioQuery.synthesis(0);
+    await audioQuery.synthesis(0, {});
 }
 
 main();

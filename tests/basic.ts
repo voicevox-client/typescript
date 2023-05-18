1f78bb5a81a2ffdb0c2fb449771204a0c4701e29
mc_fdc <Masato.04.11.2007@gmail.com>
2023-05-18 17:31:43 +0900

    Fix test bug

--- tests/basic.ts
+++ tests/basic.ts
@@ -5,8 +5,7 @@ const client = new Client("http://127.0.0.1:50021");
 
 async function main() {
     const audioQuery = await client.createAudioQuery("こんにちは", 0, {});
-    const data = await audioQuery.synthesis(0);
-    await fs.writeFile('sample.wav', data);
+    await audioQuery.synthesis(0);
 }
 
 main();

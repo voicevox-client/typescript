a633c3eb6ec348395b20bb304066361034db0ca5
mc_fdc <Masato.04.11.2007@gmail.com>
2023-05-18 17:44:18 +0900

    basic.ts を更新

--- tests/basic.ts
+++ tests/basic.ts
@@ -4,8 +4,8 @@ import { promises as fs } from "fs";
 const client = new Client("http://127.0.0.1:50021");
 
 async function main() {
-    const audioQuery = await client.createAudioQuery("こんにちは", 0, {});
-    await audioQuery.synthesis(0, {});
+    const audioQuery = await client.createAudioQuery("こんにちは", 0);
+    await audioQuery.synthesis(0);
 }
 
 main();
